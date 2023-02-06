cdc08405001a27f28af6a70869886fd8994482d9
Ilia Lataria <lataria.dev@gmail.com>
2023-02-06 14:30:09 +0400

    Product model

---
+++ Assets/Scripts/ProductModel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace MVCPractice.Models.Products
+{
+    public class ProductModel
+    {
+        private string id;
+        private string description;
+        private Sprite icon;
+
+        public ProductModel(string id, string description, Sprite icon)
+        {
+            this.id = id;
+            this.icon = icon;
+            this.description = description;
+        }
+    }
+}
+
